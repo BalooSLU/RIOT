e560aae12951d164f51eb26f071b7c3f826bc9d0
BalooSLU <prinzslu@gmail.com>
2019-10-21 18:22:21 +0200

    First data can be shown on the display. Everything is static on place. Next-> change static values to variables

--- Display_SPI/main.c
+++ Display_SPI/main.c
@@ -26,8 +26,6 @@
 #include <string.h>
 
 #include "thread.h"
-#include "shell.h"
-#include "shell_commands.h"
 
 #include "xtimer.h"
 #include "periph/gpio.h"
@@ -35,9 +33,9 @@
 #include "ucg.h"
 
    static gpio_t pins[] = {
-	[UCG_PIN_CS] = SPI0_CS0,
-	[UCG_PIN_CD] = GPIO14,
-	[UCG_PIN_RST] = GPIO2
+	[UCG_PIN_CS] = GPIO5,			// Chip select
+	[UCG_PIN_CD] = GPIO14,			// Command/Data or A0 
+	[UCG_PIN_RST] = GPIO2			// Reset pin
     };
 
    static uint32_t pins_enabled = (
@@ -45,7 +43,15 @@
 	(1 << UCG_PIN_CD) +
 	(1 << UCG_PIN_RST)
     );
-
+#define TEXT_LU "Temperatur:"	//text left up
+#define TEXT_RU	"Humidity:"	//text right up
+#define TEXT_LD "Time:"		//text left down
+#define TEXT_RD "State:"		//text right down
+#define DISP_WIDTH 128
+#define DISP_HIGHT 128
+#define TITLE_HIGHT 6
+#define TEXT_OFFSET_W 2
+#define TEXT_OFFSET_H 4
 int main(void)
 {
     uint32_t screen = 0;
@@ -61,28 +67,45 @@ int main(void)
     puts("Initializing display.");
 
     ucg_SetFontMode(&ucg, UCG_FONT_MODE_TRANSPARENT);
-    ucg_SetFont(&ucg, ucg_font_helvB12_tf);
 
     /* start drawing in a loop */
-    puts("Drawing on screen.");
-
+    puts("Drawing on Display.");
+	
+    ucg_ClearScreen(&ucg);
+    ucg_SetFontPosBaseline(&ucg);
     while (1) {
-        ucg_ClearScreen(&ucg);
+       
 
         switch (screen) {
             case 0:
+		// Start up Display
                 ucg_SetColor(&ucg, 0, 189, 32, 43);
-                ucg_DrawString(&ucg, 0, 20, 0, "THIS");
-                break;
+		ucg_SetFont(&ucg, ucg_font_timB08_hf);	// change font 
+		// Upper part                
+		ucg_DrawString(&ucg, TEXT_OFFSET_W, (TITLE_HIGHT + TEXT_OFFSET_H), 0, TEXT_LU);
+		ucg_DrawRFrame(&ucg, 0, 0, DISP_WIDTH/2, DISP_HIGHT/2, 3);
+		ucg_DrawRFrame(&ucg, DISP_WIDTH/2, 0, DISP_WIDTH/2, DISP_HIGHT/2, 3);
+		uint32_t width = (uint32_t) DISP_WIDTH - (ucg_GetStrWidth(&ucg, TEXT_RU)+TEXT_OFFSET_W);
+		ucg_DrawString(&ucg, width, (TITLE_HIGHT + TEXT_OFFSET_H), 0, TEXT_RU);
+		// Down part		
+		ucg_DrawRFrame(&ucg, 0,DISP_HIGHT/2,DISP_WIDTH/2 , DISP_HIGHT/2, 3);
+		ucg_DrawRFrame(&ucg, (DISP_WIDTH/2), DISP_HIGHT/2, DISP_WIDTH/2, DISP_HIGHT/2, 3);
+	
+		ucg_DrawString(&ucg, TEXT_OFFSET_W, ((DISP_HIGHT/2) + TITLE_HIGHT + TEXT_OFFSET_H), 0, TEXT_LD);
+
+		width = (uint32_t) DISP_WIDTH - (ucg_GetStrWidth(&ucg, TEXT_RD) + TEXT_OFFSET_W);
+		ucg_DrawString(&ucg, width, ((DISP_HIGHT/2) + TITLE_HIGHT + TEXT_OFFSET_H), 0, TEXT_RD);
+       		/* show screen in next iteration */
+        	screen += 1;
+	 	break;
             case 1:
+		ucg_SetFont(&ucg, ucg_font_helvB12_tf);	// change font
                 ucg_SetColor(&ucg, 0, 63, 167, 136);
-                ucg_DrawString(&ucg, 0, 20, 0, "IS");
+                ucg_DrawString(&ucg, TEXT_OFFSET_W+ 10, 50, 0, "35C");
                 break;
         }
 
 
-        /* show screen in next iteration */
-        screen = (screen + 1) % 2;
 
         /* sleep a little */
         xtimer_sleep(1);
