3085fa7946193120d6d7a44d9d93d1aa189873a8
BalooSLU <prinzslu@gmail.com>
2019-11-04 18:22:13 +0100

    Cleaned up code

--- Display_SPI/main.c
+++ Display_SPI/main.c
@@ -22,26 +22,35 @@
 #include "periph/gpio.h"
 #include "periph/spi.h"
 #include "ucg.h"
+#include "logo.h"
 
-#define TEXT_UL "Temperatur:"	//text left up
-#define TEXT_UR	"Humidity:"	//text right up
+#define TEXT_UL "Temp:"		//text left up
+#define TEXT_UR	"Humi:"		//text right up
 #define TEXT_DL "Time:"		//text left down
 #define TEXT_DR "State:"	//text right down
 #define DISP_WIDTH 128		//display width
 #define DISP_HIGHT 128		//display hight
-#define TITLE_HIGHT 6		//groesse der Titelschrift
-#define TEXT_OFFSET_W 2		//die Rahmen um die boxen erzeugen einen offset wo keine Schrift rein kann. (in der breite)
-#define TEXT_OFFSET_H 4		//die Rahmen um die boxen erzeugen einen offset wo keine Schrift rein kann. (in der hoehe)
+#define VAR_TEXT_HIGHT 12	//groesse der Variablenschrift
+#define TITLE_TEXT_HIGHT 8	//groesse der Titelschrift
+#define FRAME_EGDE_RADIUS 3	//Radius for the four edges.
+#define TEXT_OFFSET_W 3		//die Rahmen um die boxen erzeugen einen offset wo keine Schrift rein kann. (in der breite)
+#define TEXT_OFFSET_H 3		//die Rahmen um die boxen erzeugen einen offset wo keine Schrift rein kann. (in der hoehe)
+#define COLS 2
+#define LINES 2
+#define FRAME_WIDTH (DISP_WIDTH / COLS)
+#define FRAME_HIGHT (DISP_HIGHT / LINES)
+#define VAR_TEXT_LENGTH_MAX (FRAME_WIDTH - 2 *TEXT_OFFSET_W)   
+#define VAR_TEXT_POSITION_H (FRAME_HIGHT/2 + VAR_TEXT_HIGHT)
 
-void title_ul(ucg_t* ucg, char text[]);
-void title_ur(ucg_t* ucg, char text[]);
-void title_dl(ucg_t* ucg, char text[]);
-void title_dr(ucg_t* ucg, char text[]);
+// text_check MODES
+#define MODE_VARIABLE 1
+#define MODE_TITLE    2
 
-void variable_ul(ucg_t* ucg, char text[]);
-void variable_ur(ucg_t* ucg, char text[]);
-void variable_dl(ucg_t* ucg, char text[]);
-void variable_dr(ucg_t* ucg, char text[]);
+void frame(ucg_t* ucg, int line, int colum);
+void title(ucg_t* ucg, char text[], int line, int colum);
+void variable(ucg_t* ucg, char text[], int line, int colum);
+void clearVar(ucg_t* ucg, int line, int colum);
+void check_text(ucg_t* ucg, char *quelle, char *ziel);
 
 static gpio_t pins[] = {
 	[UCG_PIN_CS] = GPIO5,			// Chip select
@@ -92,26 +101,41 @@ int main(void)
        
         switch (screen) {
             case 0:
+		ucg_ClearScreen(&ucg);
 		// Start up Display
-		 ucg_SetColor(&ucg, 0, 189, 32, 43);
-
-		title_ul(&ucg, TEXT_UL);
-		//title_ur(&ucg, TEXT_UR);
-		//title_dl(&ucg, TEXT_DL);
-		title_dr(&ucg, TEXT_DR);
+		frame(&ucg, 0, 0);
+		frame(&ucg, 0, 1);
+		frame(&ucg, 1, 0);
+		frame(&ucg, 1, 1);
+		title(&ucg, TEXT_UL, 0, 0);
+		title(&ucg, TEXT_UR, 0, 1);
+		title(&ucg, TEXT_DL, 1, 0);
+		title(&ucg, TEXT_DR, 1, 1);
        		/* show screen in next iteration */
         	screen += 1;
 	 	break;
             case 1:
-		// Start up Display
-		// change Color	
-		ucg_SetColor(&ucg, 0, 63, 167, 136);
-		variable_ul(&ucg, "35 C");
-		screen += 1;		
+		variable(&ucg, "1234567889", 0,0);
+		variable(&ucg, "2", 0,1);
+		variable(&ucg, "3", 1,0);
+		variable(&ucg, "4", 1,1);
+		screen = 0;		
                 break;
 	    case 2:
-		variable_ul(&ucg, " ");
+		break;
+	     case 3:
+		//Riot Logo test only
+		ucg_ClearScreen(&ucg);
+		for (int y = 0; y < 48; y++) {
+                    for (int x = 0; x < 96; x++) {
+                        uint32_t offset = (x + (y * 96)) * 3;
+
+                        ucg_SetColor(&ucg, 0, logo[offset + 2], logo[offset + 1],logo[offset + 0] );
+                        ucg_DrawPixel(&ucg, x, y);
+                    }
+		}
 		screen = 0;
+		break;
         }
 
 
@@ -122,109 +146,72 @@ int main(void)
 
     return 0;
 }
+//1. Draw Frame 
+void frame(ucg_t* ucg, int line, int colum)
+{	
+	ucg_SetColor(ucg, 0, 189, 32, 43);
+	// draw frame
+	ucg_DrawRFrame(ucg, colum * FRAME_WIDTH,  line * FRAME_HIGHT, FRAME_WIDTH, FRAME_HIGHT, FRAME_EGDE_RADIUS);
+}
 			// Text Position
-			/*      -------------------
-			*  	- 1.	-	2.-
-			*	-------------------
-			*  	- 3.	-	4.-
-			*	-------------------
+			/*      --------------------
+			*  	- 0.0	-	0.1-
+			*	--------------------
+			*  	- 1.0	-	1.1-
+			*	--------------------
 			*/
-//1. Title Upper Left
-void title_ul(ucg_t* ucg, char text[])
+//2. Title Text 
+void title(ucg_t* ucg, char text[], int line, int colum)
 {	
+	ucg_SetColor(ucg, 0, 189, 32, 43);	
 	// change font
-	ucg_SetFont(ucg, ucg_font_timB08_hf);	
-	// draw frame
-	ucg_DrawRFrame(ucg, 0, 0, DISP_WIDTH/2, DISP_HIGHT/2, 3);
+	ucg_SetFont(ucg, ucg_font_timB08_hf);		
 	// draw text
-	ucg_DrawString(ucg, TEXT_OFFSET_W, (TITLE_HIGHT + TEXT_OFFSET_H), 0, text);
+	ucg_DrawString(ucg, colum * FRAME_WIDTH + TEXT_OFFSET_W, line * FRAME_HIGHT + (TITLE_TEXT_HIGHT + TEXT_OFFSET_H), 0, text);
 }
-
-//2. Title Upper Right
-void title_ur(ucg_t* ucg, char text[])
-{	
-	// change font
-	ucg_SetFont(ucg, ucg_font_timB08_hf);	
-	// draw frame
-	ucg_DrawRFrame(ucg, DISP_WIDTH/2, 0, DISP_WIDTH/2, DISP_HIGHT/2, 3);
-	// draw text
-	uint32_t position = (uint32_t) DISP_WIDTH - (ucg_GetStrWidth(ucg, text)+TEXT_OFFSET_W);
-	ucg_DrawString(ucg, position, (TITLE_HIGHT + TEXT_OFFSET_H), 0, text);	
-
-}
-
-//3. Title Down Left
-void title_dl(ucg_t* ucg, char text[])
-{	
-	// change font 
-	ucg_SetFont(ucg, ucg_font_timB08_hf);	
-	// draw frame
-	ucg_DrawRFrame(ucg, 0,DISP_HIGHT/2,DISP_WIDTH/2 , DISP_HIGHT/2, 3);
-	// draw text
-	ucg_DrawString(ucg, TEXT_OFFSET_W, ((DISP_HIGHT/2) + TITLE_HIGHT + TEXT_OFFSET_H), 0, text);
-}
-//4. Title Down Right
-void title_dr(ucg_t* ucg, char text[])
-{	
-	// change font 
-	ucg_SetFont(ucg, ucg_font_timB08_hf);	
-	// draw frame	
-	ucg_DrawRFrame(ucg, (DISP_WIDTH/2), DISP_HIGHT/2, DISP_WIDTH/2, DISP_HIGHT/2, 3);
-	// draw text	
-	uint32_t position = (uint32_t) DISP_WIDTH - (ucg_GetStrWidth(ucg, text) + TEXT_OFFSET_W);
-	ucg_DrawString(ucg, position, ((DISP_HIGHT/2) + TITLE_HIGHT + TEXT_OFFSET_H), 0, text);
-}
-
 
 			// Variable Position
-			/*      -------------------
-			*  	- 1.1	-      2.1-
-			*	-------------------
-			*  	- 3.1	-      4.1-
-			*	-------------------
+			/*      --------------------
+			*  	- 0.0	-	0.1-
+			*	--------------------
+			*  	- 1.0	-	1.1-
+			*	--------------------
 			*/
-//1.1 Variable Upper Left
-void variable_ul(ucg_t* ucg, char text[])
+//*3. Variable Text
+void variable(ucg_t* ucg, char text[], int line, int colum)
 {	
-	if(text
+	//clear area
+	clearVar(ucg, line, colum);	
+	// change Color	
+	ucg_SetColor(ucg, 0, 63, 167, 136);	
 	// change font
-	ucg_SetFont(ucg, ucg_font_helvB12_tf);	
-	// draw text
-	ucg_DrawString(ucg, TEXT_OFFSET_W+ 10, 50, 0, text);
-
-
+	ucg_SetFont(ucg, ucg_font_helvB12_tf);
+	char checked_text[] = " ";
+	check_text(ucg, text, checked_text);
+	ucg_DrawString(ucg, colum * FRAME_WIDTH + TEXT_OFFSET_W, line * FRAME_HIGHT + VAR_TEXT_POSITION_H, 0, checked_text);
 }
-//2.1 Variable Upper Right
-void variable_ur(ucg_t* ucg, char text[])
+
+void clearVar(ucg_t* ucg, int line, int colum)
+{
+	ucg_SetColor(ucg, 0, 0, 0, 0);
+	ucg_DrawBox(ucg, colum * FRAME_WIDTH + TEXT_OFFSET_W, line * FRAME_HIGHT + (VAR_TEXT_POSITION_H - VAR_TEXT_HIGHT), VAR_TEXT_LENGTH_MAX, VAR_TEXT_HIGHT);
+}
+
+void check_text(ucg_t* ucg, char *quelle, char *ziel)
 {	
-	// change font
-	ucg_SetFont(ucg, ucg_font_timB08_hf);	
-	// draw frame
-	ucg_DrawRFrame(ucg, DISP_WIDTH/2, 0, DISP_WIDTH/2, DISP_HIGHT/2, 3);
-	// draw text
-	uint32_t width = (uint32_t) DISP_WIDTH - (ucg_GetStrWidth(ucg, text)+TEXT_OFFSET_W);
-	ucg_DrawString(ucg, width, (TITLE_HIGHT + TEXT_OFFSET_H), 0, text);	
-
-}
-
-//3.1 Variable Down Left
-void variable_dl(ucg_t* ucg, char text[])
-{	
-	// change font 
-	ucg_SetFont(ucg, ucg_font_timB08_hf);	
-	// draw frame
-	ucg_DrawRFrame(ucg, 0,DISP_HIGHT/2,DISP_WIDTH/2 , DISP_HIGHT/2, 3);
-	// draw text
-	ucg_DrawString(ucg, TEXT_OFFSET_W, ((DISP_HIGHT/2) + TITLE_HIGHT + TEXT_OFFSET_H), 0, text);
-}
-//4.1 Variable Down Right
-void variable_dr(ucg_t* ucg, char text[])
-{	
-	// change font 
-	ucg_SetFont(ucg, ucg_font_timB08_hf);	
-	// draw frame	
-	ucg_DrawRFrame(ucg, (DISP_WIDTH/2), DISP_HIGHT/2, DISP_WIDTH/2, DISP_HIGHT/2, 3);
-	// draw text	
-	uint32_t width = (uint32_t) DISP_WIDTH - (ucg_GetStrWidth(ucg, text) + TEXT_OFFSET_W);
-	ucg_DrawString(ucg, width, ((DISP_HIGHT/2) + TITLE_HIGHT + TEXT_OFFSET_H), 0, text);
-}
+	char out[16];
+	strcpy(out, quelle);
+	
+	int str_length = strlen(quelle);
+	while (ucg_GetStrWidth(ucg, out) >= FRAME_WIDTH)
+	{	
+		if(str_length > 1)
+		{	
+			str_length--;
+			out[str_length] = '\0';
+			strcat(out, ".");
+			puts(out);
+		}else break;
+	}
+ 	strcpy(ziel, out);	
+} 
