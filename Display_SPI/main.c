b3ed410faf6522c6eef700cb3740ba84b07101e6
BalooSLU <prinzslu@gmail.com>
2019-11-18 16:01:32 +0100

    Added multi pages

--- Display_SPI/main.c
+++ Display_SPI/main.c
@@ -26,18 +26,22 @@
 
 #define VAR_FONT ucg_font_7x14B_tf
 #define TITLE_FONT ucg_font_timB08_hf
+// Text Beispiele:
 #define TEXT_UL "Temperatur:"	//text left up
 #define TEXT_UR	"Humitity:"	//text right up
 #define TEXT_DL "Time:"		//text left down
 #define TEXT_DR "State:"	//text right down
+// Display Eigenschaften
 #define DISP_WIDTH 128		//display width
 #define DISP_HIGHT 128		//display hight
+// Text Eigenschaften
 #define VAR_TEXT_HIGHT 10	//groesse der Variablenschrift
 #define VAR_TEXT_PLUS 4		//unterer Bereich von "gyqj"
 #define TITLE_TEXT_HIGHT 8	//groesse der Titelschrift
-#define FRAME_EGDE_RADIUS 3	//Radius for the four edges.
 #define TEXT_OFFSET_W 3		//die Rahmen um die boxen erzeugen einen offset wo keine Schrift rein kann. (in der breite)
 #define TEXT_OFFSET_H 3		//die Rahmen um die boxen erzeugen einen offset wo keine Schrift rein kann. (in der hoehe)
+// Box Eigenschaften
+#define FRAME_EGDE_RADIUS 3	//Radius for the four edges.
 #define COLS 2			//Anzahl der boxen (in der breite)
 #define LINES 2			//Anzahl der boxen (in der hoehe)
 #define QBOXES (COLS * LINES) 	//Anzahl der boxen (pro page)
@@ -45,7 +49,6 @@
 #define FRAME_HIGHT (DISP_HIGHT / LINES)//groesse der boxen (in der hoehe)
 #define VAR_TEXT_LENGTH_MAX (FRAME_WIDTH - 2 *TEXT_OFFSET_W) // maximale Anzahl an Pixel für eine Variable (in der breite)   
 #define VAR_TEXT_POSITION_H (FRAME_HIGHT/2 + VAR_TEXT_HIGHT) // hoehe wo die Variable hingeschrieben wird
-
 // text_check() MODES
 #define MODE_VARIABLE 0x33
 #define MODE_TITLE    0x32
@@ -59,15 +62,18 @@ void title(ucg_t* ucg, char text[], int line, int colum);
 void variable(ucg_t* ucg, char text[], int line, int colum);
 void clearVar(ucg_t* ucg, int line, int colum);
 void check_text(ucg_t* ucg, char *quelle, char *ziel, char mode);
-void changePage(uint8_t page);
+void changePage(ucg_t* ucg, uint8_t page);
+void init_disp(ucg_t* ucg);
+void init_pins(void);
 
-typedef struct {
+typedef struct showText {
 char title[50];
 char variable[50];
 int line;
 int colum;
 int page;
-}showText;
+struct showText * next;
+};
 
 static showText showTex[100];
 static uint8_t nr = 0;		// letzter Eintrag in die Struktur 
@@ -100,51 +106,37 @@ int main(void)
     uint32_t screen = 0;
     ucg_t ucg;
 
-
-    ucg_SetPins(&ucg, pins, pins_enabled);
-    ucg_SetDevice(&ucg, SPI_DEV(0));
-    
-    ucg_Init(&ucg, ucg_dev_st7735_18x128x160, ucg_ext_st7735_18, ucg_com_riotos_hw_spi);
- 
-/* initialize the display */
-    puts("Initializing display.");
-
-    ucg_SetFontMode(&ucg, UCG_FONT_MODE_TRANSPARENT);
-
-    /* start drawing in a loop */
-    puts("Drawing on Display.");
-
-    ucg_ClearScreen(&ucg);
-    ucg_SetFontPosBaseline(&ucg);
+    init_disp(&ucg);
+    init_pins();
     
     addParam(TEXT_UL, "1234567889");
     addParam(TEXT_UR, "2");
     addParam(TEXT_DL, "3");
     addParam(TEXT_DR, "4");
-    addParam("nextSite", "5");
+    addParam("NextSite", "5");
+    addParam("FullSide", "9");
+    addParam("TestSide", "913");
+    addParam("Te1de", "903");
+	addParam("Te2ide", "91");
+addParam("Te3e", "91");
+addParam("Tes4ide", "91");
+addParam("T5Side", "91");
+addParam("T6ide", "913");
+addParam("Te7ide", "913");
+
     while (1) {
        
         switch (screen) {
             case 0:
-		//ucg_ClearScreen(&ucg);
-		// Start up Display
-		drawFrames(&ucg, 0);
-		drawTitles(&ucg, 0);
-		drawVariables(&ucg, 0);
-
+		changePage(&ucg, 0);
         	screen += 1;
 	 	break;
             case 1:
-		ucg_ClearScreen(&ucg);
-		if(max_page >= 1)
-		{
-			drawFrames(&ucg, 1);
-			//drawTitles(&ucg, 1);
-			//drawVariables(&ucg, 1);
-		}
+		changePage(&ucg, 1);
 		screen += 1;		
                 break;
 	    case 2:
+		changePage(&ucg, 2);
 		screen = 0;
 		break;
 	     case 3:
@@ -165,19 +157,37 @@ int main(void)
         /* sleep a little */
         xtimer_sleep(1);
     }
-
     return 0;
 }
+void init_pins(void)
+{
+	
+}
+void init_disp(ucg_t* ucg)
+{
+    ucg_SetPins(ucg, pins, pins_enabled);
+    ucg_SetDevice(ucg, SPI_DEV(0));
+    
+    ucg_Init(ucg, ucg_dev_st7735_18x128x160, ucg_ext_st7735_18, ucg_com_riotos_hw_spi);
+ 
+/* initialize the display */
+    puts("Initializing display.");
+    ucg_SetFontMode(ucg, UCG_FONT_MODE_TRANSPARENT);
+
+    ucg_ClearScreen(ucg);
+    ucg_SetFontPosBaseline(ucg);	
+}
+// funktion um die Struktur richtig zu füllen.
 void addParam(char title[], char variable[])
 {	
 	uint8_t page = 0;				// Anzahl der Seiten
 	uint8_t position = 0;				// Position auf der Seite
 	
-	if(nr >= QBOXES)
+	if(nr >= QBOXES)				// Ob naechste Parameter auf neue Seite muss  
 	{ 
 		page = (uint8_t) (nr / QBOXES);		
 		position = (uint8_t) (nr % QBOXES); 	
-	}else
+	}else						// Seite null
 	{	page = 0;
 		position = nr;
 	}
@@ -191,33 +201,35 @@ void addParam(char title[], char variable[])
 	}else
 	{	
 		showTex[nr].line = (uint8_t) (position / COLS);
-		showTex[nr].colum = (uint8_t) ((position % COLS)-1); 
+		showTex[nr].colum = (uint8_t) (position % COLS); 
 	}
+	//printf("Position/Page: %d/%d\n Line: %d \n  Colum: %d\n",position, page, showTex[nr].line, showTex[nr].colum);
 	showTex[nr].page = page;
 	max_page = page;
-	printf("%d\n", nr);
 	nr ++;
 }
-//** 
+
 void drawFrames(ucg_t* ucg, uint8_t inputpage)
 {	
-	uint8_t line = 0;
-	uint8_t cols = 0;
+	uint8_t l = 0;
+	uint8_t c = 0;
 	
 	if(inputpage < max_page)				// full Page
 	{
-		line = LINES;
-		cols = COLS;
+		for(l=0; l < LINES; l++)
+		{
+			for(c=0; c < COLS; c++)
+			{
+				frame(ucg, l, c);
+			}
+		}
 	}else if(inputpage == max_page)				// teils full
 	{
-		uint8_t position = (uint8_t) ((nr + 1) % QBOXES); //<---*
-		line = (uint8_t) (position / COLS);
-		cols = (uint8_t) ((position % COLS)-1); 
-	}
-	for(int l=0; l < line; l++)
-	{
-		for(int c=0; c < cols; c++)
+		uint8_t position = (uint8_t) ((nr-1) % QBOXES); // nr -1 == actual position
+		for(int p = 0; p <= position; p++)
 		{
+			l = (uint8_t) (p / COLS);
+			c = (uint8_t) (p % COLS);
 			frame(ucg, l, c);
 		}
 	}
@@ -225,30 +237,65 @@ void drawFrames(ucg_t* ucg, uint8_t inputpage)
 
 void drawTitles(ucg_t* ucg, uint8_t inputpage)
 {
-	for(int l=0; l < LINES; l++)
+	uint8_t l = 0;
+	uint8_t c = 0;
+	
+	if(inputpage < max_page)				// full Page
 	{
-		for(int c=0; c < COLS; c++)
+		for(l=0; l < LINES; l++)
 		{
-			title(ucg,showTex[(l*COLS)+c].title, l, c);
+			for(c=0; c < COLS; c++)
+			{
+				title(ucg,showTex[(l*COLS)+c+inputpage*QBOXES].title, l, c);
+			}
+		}
+	}else if(inputpage == max_page)				// teil Page
+	{
+		uint8_t position = (uint8_t) ((nr-1) % QBOXES); // nr -1 == actual position
+		for(int p = 0; p <= position; p++)
+		{
+			l = (uint8_t) (p / COLS);
+			c = (uint8_t) (p % COLS);
+			title(ucg,showTex[(l*COLS)+c+inputpage*QBOXES].title, l, c);
 		}
 	}
 }
 void drawVariables(ucg_t* ucg, uint8_t inputpage)
 {
-	for(int l=0; l < LINES; l++)
+	uint8_t l = 0;
+	uint8_t c = 0;
+	
+	if(inputpage < max_page)				// full Page
 	{
-		for(int c=0; c < COLS; c++)
+		for(l=0; l < LINES; l++)
 		{
-			variable(ucg,showTex[(l*COLS)+c].variable, l, c);
+			for(c=0; c < COLS; c++)
+			{
+				variable(ucg,showTex[(l*COLS)+c+inputpage*QBOXES].variable, l, c);
+			}
 		}
-	}	
+	}else if(inputpage == max_page)				// teil Page
+	{
+		uint8_t position = (uint8_t) ((nr-1) % QBOXES); // nr -1 == actual position
+		for(int p = 0; p <= position; p++)
+		{
+			l = (uint8_t) (p / COLS);
+			c = (uint8_t) (p % COLS);
+			variable(ucg,showTex[(l*COLS)+c+inputpage*QBOXES].variable, l, c);
+		}
+	}
 }
-void changePage(uint8_t page)
+void changePage(ucg_t* ucg, uint8_t page)
 {
-
+	if(page <= max_page)
+	{	
+		ucg_ClearScreen(ucg);
+		drawFrames(ucg, page);
+		drawTitles(ucg, page);
+		drawVariables(ucg, page);
+	}
 }
 
-//1. Draw Frame 
 void frame(ucg_t* ucg, int line, int colum)
 {	
 	ucg_SetColor(ucg, 0, 189, 32, 43);
@@ -262,7 +309,7 @@ void frame(ucg_t* ucg, int line, int colum)
 			*  	- 1.0	-	1.1-
 			*	--------------------
 			*/
-//2. Title Text 
+ 
 void title(ucg_t* ucg, char text[], int line, int colum)
 {	
 	ucg_SetColor(ucg, 0, 189, 32, 43);	
@@ -282,7 +329,7 @@ void title(ucg_t* ucg, char text[], int line, int colum)
 			*  	- 1.0	-	1.1-
 			*	--------------------
 			*/
-//3. Variable Text
+
 void variable(ucg_t* ucg, char text[], int line, int colum)
 {	
 	//clear area
